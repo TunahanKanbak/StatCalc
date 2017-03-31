f678395c7f09ce04e8f30681448ee63c7b7670a1
TunahanKanbak <TunahanKanbak@users.noreply.github.com>
2017-03-31 12:20:37 +0300

    Update Statical Calculations.py
    -Gaussian distribution for all data set

--- Statical Calculations.py
+++ Statical Calculations.py
@@ -1,5 +1,7 @@
 import math
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def average(val):
     sum_value = 0
@@ -9,7 +11,7 @@ def average(val):
     return mean
 
 
-def standard_deviation(mean,val):
+def standard_deviation(mean, val):
     sum_value = 0
     for i in range(len(val)):
         sum_value += (val[i] - mean)**2
@@ -17,13 +19,35 @@ def standard_deviation(mean,val):
     return sum_value
 
 
-def standard_deviation2(mean, val):
+def standard_deviation2(val, mean):
     sum_value = 0
     for i in range(len(val)):
         sum_value += (val[i] - mean)**2
     sum_value = math.sqrt(sum_value/(len(val)))
     return sum_value
 
+
+def q_critical(data_set,conf_level):
+    Q_test_confidence = {"90": [0.941, 0.765, 0.642, 0.560, 0.507, 0.468, 0.437, 0.412, 0.338, 0.3, 0.277, 0.26],
+                         "95": [0.970, 0.829, 0.710, 0.625, 0.568, 0.526, 0.493, 0.466, 0.384, 0.342, 0.317, 0.298],
+                         "99": [0.994, 0.926, 0.821, 0.740, 0.680, 0.634, 0.598, 0.568, 0.475, 0.425, 0.393, 0.372]}
+    selected_conf_level = str(int(conf_level))
+    Q_test_nums = Q_test_confidence[selected_conf_level]
+    if len(data_set) <= 10:
+        q_crit = Q_test_nums[len(data_set)-3]
+        return q_crit
+    elif len(data_set) <= 15:
+        q_crit = Q_test_nums[8]
+        return q_crit
+    elif len(data_set) <= 20:
+        q_crit = Q_test_nums[9]
+        return q_crit
+    elif len(data_set) <= 25:
+        q_crit = Q_test_nums[10]
+        return q_crit
+    elif len(data_set) <= 30:
+        q_crit = Q_test_nums[11]
+        return q_crit
 
 def q_test_last(val, q_crit):
     val.sort()
@@ -39,38 +63,33 @@ def q_test_first(val, q_crit):
         return True
 
 
-def gaussian_dist(mean,val):
-    std = standard_deviation2(val,mean)
-    x = mean - 4 * round(std, 2)
-    counter = int(100*8*round(std, 2))
-    ver = []
-    hor = []
-    for i in range(counter):
-        f = (1 / ((2 * math.pi * (0.11595 ** 2)) ** 0.5)) * (
-        math.exp((-(round(x, 2) - 2.47) ** 2) / (2 * 0.11595 * 0.11595)))
-        ver.append(f)
-        hor.append(x)
-        x += 0.01
+def gaussian_dist(mean, std, x):
+    return np.exp(-np.power(x - mean, 2.) / (2 * np.power(std, 2.)))
 
-    plt.plot(hor, ver)
+
+def gaussian_plot(mean, name, std):
+    x = np.linspace(round((mean - 3*std)), round((mean + 3*std)), num=60)
+    y = gaussian_dist(mean, std, np.linspace(round((mean - 3*std)), round((mean + 3*std)), num=60))
+    fig = plt.figure()
+    plt.plot(x, y)
     plt.ylabel("Gaussian Distribution")
     plt.xlabel("Length - cm")
-    plt.show()
+    fig.savefig(name)
+    plt.close()
+    print(name)
 
 
-Q_test_confidence={"90" : [0.941, 0.765, 0.642, 0.560, 0.507, 0.468, 0.437, 0.412], "95": [0.970, 0.829, 0.710, 0.625, 0.568, 0.526, 0.493, 0.466], "99": [0.994, 0.926, 0.821, 0.740, 0.680, 0.634, 0.598, 0.568]}
 f_var = open('input.txt')
 all_values = [list(map(float, line.split(" "))) for line in f_var]  #read each line an creates a data matrix
 f_var.close()
 f_name = open('names.txt')
-names = [str(line) for line in f_name]  #read each line an creates a name matrix
+names = [str(line.strip("\n")) for line in f_name]  #read each line an creates a name matrix
 f_name.close()
 for i in range(1, len(all_values)):
-    selected_conf_level = str(int(all_values[0][0]))
-    Q_test_nums = Q_test_confidence[selected_conf_level]
-    q_crit = Q_test_nums[len(all_values[i])-3]
     count_q_last = 0
     count_q_first = 0
+    q_crit = q_critical(all_values[i],all_values[0][0])
+
     while q_test_first(all_values[i], q_crit) or q_test_last(all_values[i], q_crit):
         if len(all_values[i]) > 2:
             if q_test_last(all_values[i], q_crit):
@@ -81,13 +100,12 @@ for i in range(1, len(all_values)):
                 count_q_first += 1
 
     mean = average(all_values[i])
-    sample_std = standard_deviation(mean,all_values[i])
-    population_std = standard_deviation2(mean,all_values[i])
+    sample_std = standard_deviation(mean, all_values[i])
+    population_std = standard_deviation2(all_values[i], mean)
+    gaussian_plot(mean, names[i-1], population_std)
 
-    #gaussian_dist(mean, orj_values) #To see gaussian distribution curve remove first #
-
-    f = open('output.txt', 'a')
-    f.write("\n" + names[i-1])
+    f = open("output.txt", 'a')
+    f.write("\n" + names[i-1] + "\n")
     f.write("Mean of data set is " + str(round(mean,3)) + "\n")
     f.write("Sample Standard Deviation(N-1) of data set is " + str(round(sample_std,3))+ "\n")
     f.write("Population Standard Deviation(N) of data set is " + str(round(population_std, 3))+ "\n")
